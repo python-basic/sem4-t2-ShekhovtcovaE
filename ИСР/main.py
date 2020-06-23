975796d12ef8f51bf5df9b05dd201f87f189aff6
Elizaveta_Shekhovtcova <54999570+ShekhovtcovaE@users.noreply.github.com>
2020-06-23 17:35:59 +0300

    Add files via upload

---
+++ ИСР/main.py
@@ -0,0 +1,45 @@
+import functools
+
+def jr(calc):
+    @functools.wraps(calc)
+    def wrapperr():
+        string = calc()
+        if string:
+            try:
+                with open('journal.txt', 'a+') as j:
+                    j.write(string)
+            except FileNotFoundError:
+                print('Файл не найден')
+    return wrapperr
+
+@jr
+def calc():
+    s = input("Введите одно из следующих операции: +,-,*,/,^: ")
+    try:
+        x = float(input("x="))
+    except ValueError:
+        print('Неверно введен x')
+    try:
+        y = float(input("y="))
+    except ValueError:
+        print('Неверно введен y')
+    if s == '+':
+        res = x+y
+    elif s == '-':
+        res = x-y
+    elif s == '*':
+        res = x*y
+    elif s == '^':
+        res = pow(x, y)
+    elif s == '/':
+        if not y:
+            print("Деление на ноль")
+            return False
+        res = x/y            
+    else:
+        print("Неверный знак операции!")
+        return False
+    return (str(x) + s + str(y) + '=' + str(res) + '\n')
+
+while True:
+    calc()
